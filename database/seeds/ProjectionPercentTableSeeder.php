8233a92fdf82d25966dda8b7f71365184d5abf93
emigort <emigort@gmail.com>
2019-05-21 16:52:15 -0400

    seeders percent projection

---
+++ database/seeds/ProjectionPercentTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ProjectionPercentTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $from = 2016;
+        $to = 2030;
+
+
+    }
+}
