c33c51ba44063535939ba3cf252baf5845f70150
gilberto <gilbertoguedes@gmail.com>
2019-05-23 18:55:58 -0430

    meee

--- app/Models/DailyRevenue.php
+++ app/Models/DailyRevenue.php
@@ -7,6 +7,8 @@ use Illuminate\Support\Facades\DB;
 
 class DailyRevenue extends Model
 {
+    protected $table = 'weeks';
+
     public function post()
     {
         return $this->belongsTo('App\Models\StoreWeek');
@@ -22,5 +24,30 @@ class DailyRevenue extends Model
             ->where('store_week.week_id',$week_id)
             ->get();
         return  $seven_days_week;
+    }
+
+    static function lastDayWeek($store_id,$week_id)
+    {
+        $seven_days_week = DB::table('daily_revenues')
+            ->leftjoin('store_week','store_week.id','=','daily_revenues.store_week_id')
+            ->leftjoin('dates_dim','dates_dim.date','=','daily_revenues.dates_dim_date')
+            ->select('daily_revenues.*','dates_dim.*')
+            ->where('store_week.store_id',$store_id)
+            ->where('store_week.week_id',$week_id)
+            ->orderBy('dates_dim_date','desc')
+            ->first();
+        return  $seven_days_week;
+    }
+
+    static function totalAmtWeek($store_id,$week_id)
+    {
+        $total_amt_week = DB::table('daily_revenues')
+            ->leftjoin('store_week','store_week.id','=','daily_revenues.store_week_id')
+            ->leftjoin('dates_dim','dates_dim.date','=','daily_revenues.dates_dim_date')
+            ->select('daily_revenues.*','dates_dim.*')
+            ->where('store_week.store_id',$store_id)
+            ->where('store_week.week_id',$week_id)
+            ->get()->sum('amt');
+        return  $total_amt_week;
     }
 }
