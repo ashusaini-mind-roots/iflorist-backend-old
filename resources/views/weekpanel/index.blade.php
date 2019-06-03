06d0bd36cfe8920e9459c42446b4e44132438d72
Emilio Gort <egort@infsalesgroup.com>
2019-06-03 14:32:21 -0400

    centering daily revenue amts addin week end day to week list

--- resources/views/weekpanel/index.blade.php
+++ resources/views/weekpanel/index.blade.php
@@ -13,7 +13,8 @@
                                 <label for="storesid">Select store</label>
                                 <select id="storesid" class="form-control" ng-required="true"
                                         ng-model="selectedStoreItem"
-                                        ng-options="store.id as store.store_name for store in storesList">
+                                        ng-options="store.id as store.store_name for store in storesList"
+                                        ng-change="getWeekDataFromServer()">
                                 </select>
                             </div>
                             <div class="col-2">
@@ -26,7 +27,7 @@
                             <div class="col-2">
                                 <label for="weeksid">Select week</label>
                                 <select id="weeksid" class="form-control" ng-required="true" ng-model="selectedWeekItem"
-                                        ng-options="week.id as week.number for week in weekList"
+                                        ng-options='week.id as (week.number+"-"+week.week_end_day) for week in weekList'
                                         ng-change="getWeekDataFromServer()">
                                 </select>
                             </div>
@@ -44,75 +45,75 @@
                         <h5 class="card-title">Actual daily revenue</h5>
                         <div class="row">
                             <div class="col">
-                                <div class="form-group">
+                                <div class="form-group text-center">
                                     <span>@{{monday.dates_dim_date | date:'MM/dd/yyyy'}}</span><br>
                                     <label for="inputMonday">Monday</label>
                                     <input type="text"
-                                           class="form-control form-control-plaintext font-weight-bold"
+                                           class="form-control form-control-plaintext text-center font-weight-bold"
                                            id="inputMonday" ng-model="monday.amt_formatted"
                                            data-type="currency"
                                     >
                                 </div>
                             </div>
                             <div class="col">
-                                <div class="form-group">
+                                <div class="form-group text-center">
                                     <span>@{{tuesday.dates_dim_date | date:'MM/dd/yyyy'}}</span><br>
                                     <label for="inputTuesday">Tuesday</label>
                                     <input type="text"
-                                           class="form-control form-control-plaintext font-weight-bold"
+                                           class="form-control form-control-plaintext text-center font-weight-bold"
                                            id="inputTuesday" ng-model="tuesday.amt_formatted">
                                 </div>
                             </div>
                             <div class="col">
-                                <div class="form-group">
+                                <div class="form-group text-center">
                                     <span>@{{wednesday.dates_dim_date | date:'MM/dd/yyyy'}}</span><br>
                                     <label for="inputWednesday">Wednesday </label>
                                     <input type="text"
-                                           class="form-control form-control-plaintext font-weight-bold"
+                                           class="form-control form-control-plaintext text-center font-weight-bold"
                                            id="inputWednesday" ng-model="wednesday.amt_formatted">
                                 </div>
                             </div>
                             <div class="col">
-                                <div class="form-group">
+                                <div class="form-group text-center">
                                     <span>@{{thursday.dates_dim_date | date:'MM/dd/yyyy'}}</span><br>
                                     <label for="inputThursday">Thursday </label>
                                     <input type="text"
-                                           class="form-control form-control-plaintext font-weight-bold"
+                                           class="form-control form-control-plaintext text-center font-weight-bold"
                                            id="inputThursday" ng-model="thursday.amt_formatted">
                                 </div>
                             </div>
                             <div class="col">
-                                <div class="form-group">
+                                <div class="form-group text-center">
                                     <span>@{{friday.dates_dim_date | date:'MM/dd/yyyy'}}</span><br>
                                     <label for="inputFriday">Friday </label>
                                     <input type="text"
-                                           class="form-control form-control-plaintext font-weight-bold"
+                                           class="form-control form-control-plaintext text-center font-weight-bold"
                                            id="inputFriday" ng-model="friday.amt_formatted">
                                 </div>
                             </div>
                             <div class="col">
-                                <div class="form-group">
+                                <div class="form-group text-center">
                                     <span>@{{saturday.dates_dim_date | date:'MM/dd/yyyy'}}</span><br>
                                     <label for="inputSaturday">Saturday </label>
                                     <input type="text"
-                                           class="form-control form-control-plaintext font-weight-bold"
+                                           class="form-control form-control-plaintext text-center font-weight-bold"
                                            id="inputSaturday" ng-model="saturday.amt_formatted">
                                 </div>
                             </div>
                             <div class="col">
-                                <div class="form-group">
+                                <div class="form-group text-center">
                                     <span>@{{sunday.dates_dim_date | date:'MM/dd/yyyy'}}</span><br>
                                     <label for="inputSunday">Sunday</label>
                                     <input type="text"
-                                           class="form-control form-control-plaintext font-weight-bold"
+                                           class="form-control form-control-plaintext text-center font-weight-bold"
                                            id="inputSunday" ng-model="sunday.amt_formatted">
                                 </div>
                             </div>
                             <div class="col">
-                                <div class="form-group">
+                                <div class="form-group text-center">
                                     <label for="inputTotal">Total </label>
                                     {{--<input hidden type="text" class="form-control" id="inputTotal" ng-model="dailyRevenueTotal" value = @{{calcDailyTotal()}} >--}}
-                                    <div class="font-weight-bold">@{{calcDailyTotal()|currency}}</div>
+                                    <div class="font-weight-bold text-center">@{{calcDailyTotal()|currency}}</div>
                                 </div>
                             </div>
                         </div>
@@ -136,8 +137,9 @@
             <div class="col-3">
                 <div class="card">
                     <div class="card-body">
+                        <h6 class="card-subtitle mb-2 text-muted">Proj. Weekly Rev.</h6>
                         <h5 class="card-title">@{{projWeeklyRev | currency}}</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">Proj. Weekly Rev.</h6><br>
+
                         {{--<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>--}}
                         {{--<a href="#" class="card-link">Card link</a>--}}
                         {{--<a href="#" class="card-link">Another link</a>--}}
@@ -147,36 +149,35 @@
             <div class="col-3">
                 <div class="card">
                     <div class="card-body">
-                        <h5 class="card-title">
-                            @{{targetCOG | number:2}} %
+                        <h6 class="card-subtitle mb-2 text-muted">Target COG's.</h6>
+                        <h5 class="card-title">@{{targetCOG | number:2}} %
                             <hr>
                             @{{ getTargetCOGInMoney() | currency }}
                         </h5>
-                        <h6 class="card-subtitle mb-2 text-muted">Target COG's.</h6>
                     </div>
                 </div>
             </div>
             <div class="col-3">
                 <div class="card">
                     <div class="card-body">
+                        <h6 class="card-subtitle mb-2 text-muted">Running COG's</h6>
                         <h5 class="card-title" ng-class=(targetCOG<calcRunningCOG())?'text-danger':''>
                             @{{calcRunningCOG() | number:2}} %
                             <hr>
                             @{{invoiceTotal|currency}}
                         </h5>
-                        <h6 class="card-subtitle mb-2 text-muted">Running COG's</h6>
                     </div>
                 </div>
             </div>
             <div class="col-3">
                 <div class="card">
                     <div class="card-body">
+                        <h6 class="card-subtitle mb-2 text-muted">Difference</h6>
                         <h5 class="card-title" ng-class="(calcCostDifference())<0?'text-danger':''">
                             @{{calcCostDifference() | number:2}}%
                             <hr>
                             @{{getTargetCOGInMoney() - invoiceTotal | currency}}
                         </h5>
-                        <h6 class="card-subtitle mb-2 text-muted">Difference</h6>
                     </div>
                 </div>
             </div>
