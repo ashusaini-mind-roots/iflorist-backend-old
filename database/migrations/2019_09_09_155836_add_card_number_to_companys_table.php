5a1e35d849fbc88c5cd7092b1d6b09023a3fd342
gilberto <gilbertoguedes@gmail.com>
2019-09-10 06:28:34 -0400

    updating

---
+++ database/migrations/2019_09_09_155836_add_card_number_to_companys_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddCardNumberToCompanysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('companys', function (Blueprint $table) {
+            $table->string('card_number',16);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('companys', function (Blueprint $table) {
+            //
+        });
+    }
+}
