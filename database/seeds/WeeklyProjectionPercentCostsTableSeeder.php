e879f29fe6bef6f303b97047a79b1684773756f9
gilberto <gilbertoguedes@gmail.com>
2019-12-04 07:51:46 -0400

    Merge branch 'DEV-CostOfGoods' of https://github.com/emigort/CostOfGood into DEV-CostOfGoods
    # Conflicts:
    Solving problems on login

--- database/seeds/WeeklyProjectionPercentCostsTableSeeder.php
+++ database/seeds/WeeklyProjectionPercentCostsTableSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use App\Models\WeeklyProjectionPercentCosts;
 
 class WeeklyProjectionPercentCostsTableSeeder extends Seeder
 {
@@ -12,10 +13,16 @@ class WeeklyProjectionPercentCostsTableSeeder extends Seeder
      */
     public function run()
     {
-        $sql = <<<EOT
+        WeeklyProjectionPercentCosts::create([
+            'target_cog' => 5,
+            'target_cof' => 20,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        /*$sql = <<<EOT
 insert into weekly_projection_percent_costs(target_cog,target_cof,created_at,updated_at)
 SELECT 5,20,now(),now() FROM store_week order by id;
 EOT;
-        DB::unprepared($sql);
+        DB::unprepared($sql);*/
     }
 }
