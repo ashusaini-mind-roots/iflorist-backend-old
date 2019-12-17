c5566f2ce8eff8452d54bd51acd2e9b93628833f
gilbertoguedes <glbrt.gds@gmail.com>
2019-12-17 13:06:59 -0500

    Migration for company employees creation table

---
+++ database/migrations/2019_12_16_230719_create_companyemployees_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCompanyemployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companyemployees', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->string('name');
+            $table->bigInteger('user_id')->unsigned()->nullable(true);
+            $table->bigInteger('company_id')->unsigned();
+
+            $table->foreign('company_id')->references('id')->on('company');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companyemployees');
+    }
+}
