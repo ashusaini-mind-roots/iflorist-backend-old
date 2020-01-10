3b30bd0e6fdb808cf26ef8df9edb61bc216cee33
gilbertoguedes <glbrt.gds@gmail.com>
2020-01-10 13:45:06 -0500

    changing some feeds

--- database/seeds/ModulesTableSeeder.php
+++ database/seeds/ModulesTableSeeder.php
@@ -15,9 +15,9 @@ class ModulesTableSeeder extends Seeder
         Module::create(['name' => 'Dashboard','number' => 1, 'icono'=> 'dashboard-icon','action'=> '/home', 'roles'=>'ROOT,COMPANYADMIN,STOREMANAGER']);
         Module::create(['name' => 'Sales','number' => 2, 'icono'=> 'sales-icon','action'=> '/sales', 'roles'=>'ROOT,COMPANYADMIN,STOREMANAGER']);
         Module::create(['name' => 'Cost of Fresh','number' => 3, 'icono'=> 'costoffresh-icon','action'=> '/cost-of/fresh', 'roles'=>'ROOT,COMPANYADMIN,STOREMANAGER']);
-        Module::create(['name' => 'Cost of Hard Goods','number' => 4, 'icono'=> 'costofgoods-icon','action'=> '/cost-of/goods', 'roles'=>'ROOT,COMPANYADMIN']);
-        Module::create(['name' => 'Sheduler','number' => 5, 'icono'=> 'scheduler-icon','action'=> '/scheduler', 'roles'=>'ROOT,COMPANYADMIN,EMPLOYEE']);
-        Module::create(['name' => 'Bloomview','number' => 6, 'icono'=> 'bloomview-icon','action'=> '#', 'roles'=>'ROOT,COMPANYADMIN']);
-        Module::create(['name' => 'Projections','number' => 7, 'icono'=> 'projections-icon','action'=> '/projections', 'roles'=>'ROOT,COMPANYADMIN']);
+        Module::create(['name' => 'Cost of Hard Goods','number' => 4, 'icono'=> 'costofgoods-icon','action'=> '/cost-of/goods', 'roles'=>'ROOT,COMPANYADMIN,STOREMANAGER']);
+        Module::create(['name' => 'Sheduler','number' => 5, 'icono'=> 'scheduler-icon','action'=> '/scheduler', 'roles'=>'ROOT,COMPANYADMIN,EMPLOYEE,STOREMANAGER']);
+        Module::create(['name' => 'Bloomview','number' => 6, 'icono'=> 'bloomview-icon','action'=> '#', 'roles'=>'ROOT,COMPANYADMIN,STOREMANAGER']);
+        Module::create(['name' => 'Projections','number' => 7, 'icono'=> 'projections-icon','action'=> '/projections', 'roles'=>'ROOT,COMPANYADMIN,STOREMANAGER']);
     }
 }
--- database/seeds/WeeklyProjectionPercentRevenueTableSeeder.php
+++ database/seeds/WeeklyProjectionPercentRevenueTableSeeder.php
@@ -14,32 +14,32 @@ class WeeklyProjectionPercentRevenueTableSeeder extends Seeder
      */
     public function run()
     {
-        WeeklyProjectionPercentRevenues::create([
-            'year_proyection' => '2019',
-            'year_reference' => '2018',
-            'amt_total' => 10,
-            'percent' => 5,
-            'store_id' => 1,
-            'week_number' => '52'
-        ]);
-
-        WeeklyProjectionPercentRevenues::create([
-            'year_proyection' => '2018',
-            'year_reference' => '2017',
-            'amt_total' => 15,
-            'percent' => 5,
-            'store_id' => 1,
-            'week_number' => '52'
-        ]);
-
-        WeeklyProjectionPercentRevenues::create([
-            'year_proyection' => '2017',
-            'year_reference' => '2016',
-            'amt_total' => 20,
-            'percent' => 5,
-            'store_id' => 1,
-            'week_number' => '52'
-        ]);
+//        WeeklyProjectionPercentRevenues::create([
+//            'year_proyection' => '2019',
+//            'year_reference' => '2018',
+//            'amt_total' => 10,
+//            'percent' => 5,
+//            'store_id' => 1,
+//            'week_number' => '52'
+//        ]);
+//
+//        WeeklyProjectionPercentRevenues::create([
+//            'year_proyection' => '2018',
+//            'year_reference' => '2017',
+//            'amt_total' => 15,
+//            'percent' => 5,
+//            'store_id' => 1,
+//            'week_number' => '52'
+//        ]);
+//
+//        WeeklyProjectionPercentRevenues::create([
+//            'year_proyection' => '2017',
+//            'year_reference' => '2016',
+//            'amt_total' => 20,
+//            'percent' => 5,
+//            'store_id' => 1,
+//            'week_number' => '52'
+//        ]);
 
     }
 }
